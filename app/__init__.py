0d9be81f41b62e25f2e52823cde871eb106b8850
Kabaki Antony <kabaki.antony@gmail.com>
2020-05-07 16:00:39 +0300

    Added blueprint to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import app_config
 from app.api.model.db import db_init
+from app.api.view.cases import globalUpdatesBlueprint as cases_blueprint
 
 
 def create_app(the_configuration):
@@ -8,6 +9,7 @@ def create_app(the_configuration):
     app = Flask(__name__)
     app.config.from_object(app_config[the_configuration])
     app.app_context().push()
+    app.register_blueprint(cases_blueprint)
     if the_configuration != "testing":
         db_init()
     return app
---
+++ app/api/__init__.py
@@ -0,0 +1,2 @@
+from flask import Blueprint
+globalUpdatesBlueprint = Blueprint('globalUpdatesBlueprint', __name__, url_prefix='')
