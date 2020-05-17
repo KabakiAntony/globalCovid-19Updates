649d952ebbc4785c3bd39736207eecfdc0b020ba
Kabaki Antony <kabaki.antony@gmail.com>
2020-05-18 00:42:36 +0300

    Removed routing for country views

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import app_config
 from app.api.model.db import db_init
 from app.api.view.cases import globalUpdatesBlueprint as cases_blueprint
 from app.api.view.users import globalUpdatesBlueprint as users_blueprint
-from app.api.view.country import globalUpdatesBlueprint as country_blueprint
 from flask_cors import CORS
 
 
@@ -15,7 +14,6 @@ def create_app(the_configuration):
     app.app_context().push()
     app.register_blueprint(cases_blueprint)
     app.register_blueprint(users_blueprint)
-    app.register_blueprint(country_blueprint)
     if the_configuration != "testing":
         db_init()
     return app
