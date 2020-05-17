0e3292d72e7b4b74cdeeb661e7d84680b0da5f33
Kabaki Antony <kabaki.antony@gmail.com>
2020-05-17 13:03:57 +0300

    added styling and js for  country upload

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -1,7 +1,8 @@
 const casesForm = document.getElementById('cases-data');
-const countryForm = document.getElementById('country-data');
 const caseFile = document.getElementById('caseFile');
+const countryForm = document.getElementById('country-data');
 const countryFile = document.getElementById('countryFile');
+
 token = localStorage.getItem('user');
 
 casesForm.addEventListener('submit',(e)=>{
@@ -22,6 +23,7 @@ casesForm.addEventListener('submit',(e)=>{
         }
         else if (status === 401)
         {
+            console.log(error,status);
             document.getElementById('cases-upload-feedback').innerText = error;
         }
         else{
@@ -29,7 +31,7 @@ casesForm.addEventListener('submit',(e)=>{
             document.getElementById('cases-upload-feedback').innerText = error;
         }    
     })
-    .catch(err => {
+    .catch((err) => {
         console.log(err)
         document.getElementById('cases-upload-feedback').innerText = err;
     });
@@ -38,29 +40,32 @@ casesForm.addEventListener('submit',(e)=>{
 countryForm.addEventListener('submit',(e)=>{
     e.preventDefault();
 
-    const form_data = new FormData();
-    form_data.append('countryCsv',countryFile.files[0]);
+    const country_data = new FormData();
+    country_data.append('countryCsv',countryFile.files[0]);
 
     fetch(`/auth/admin/upload/country?in=`+token,{
         method : 'POST',
-        body:form_data
+        body:country_data
     })
     .then(response => response.json())
     .then(({data,status,error}) => {
         if (status === 200){
+            console.log(data)
             document.getElementById('cases-upload-feedback').innerText = data;
         }
         else if (status === 401)
         {
+            console.log(error)
             document.getElementById('cases-upload-feedback').innerText = error+''+status;
         }
         else{
+            console.log(error)
             document.getElementById('cases-upload-feedback').innerText = error +''+status;
         }    
     })
-    .catch(err => {
+    .catch((err) => {
         console.log(err)
-        document.getElementById('cases-upload-feedback').innerText = err;
+        document.getElementById('cases-upload-feedback').innerText = err + "unknown error";
 
     });
 });
