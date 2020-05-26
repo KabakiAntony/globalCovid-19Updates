a14caeedcbe63f605decdeb6db51999b60895484
Kabaki Antony <kabaki.antony@gmail.com>
2020-05-26 21:05:57 +0300

    Added a global div

--- js/get.js
+++ js/get.js
@@ -1,9 +1,41 @@
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
+
+fetch('https://globalcovid19updates.herokuapp.com/global')
+.then(response=>response.json())
+.then(({data,status,error})=>{
+    if(status === 200){
+        document.getElementById('global-summary').innerHTML=`
+        <h4>Last update : ${data[0].asAt}</h4>
+        ${data.map(function(globalSummary){
+            return `
+            <label>Total confirmed cases</label>
+            <p id="confirmed-cases">${formatNumber(globalSummary.confirmedCases)}</p>
+            <label>Active cases</label>
+            <p id="active-cases">${formatNumber(globalSummary.activeCases)}</p>
+            <label>Recoveries</label>
+            <p id="recoveries">${formatNumber(globalSummary.Recoveries)}</p>
+            <label>Deaths</label>
+            <p id="deaths">${formatNumber(globalSummary.Deaths)}</p>
+            `
+        }).join('')}
+        `
+        }
+    else{
+        console.log(error, status)
+    }
+})
+.catch((err)=>{
+    console.log(err)
+
+});
 fetch('https://globalcovid19updates.herokuapp.com/global/summary')
 .then(response=>response.json())
 .then(({data,status,error})=>{
     if(status === 200){
         document.getElementById('show-updates').innerHTML=`
-        Covid-19 numbers as at ${data[0].asAt}
         <table class="holder">
             <tr>
             <th>Country</th>
